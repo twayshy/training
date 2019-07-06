b1edccda3ad6f9b7afade84ca3e0c600a89644e7
Евгения Чедка <twaylik@mail.ru>
2019-07-06 16:13:11 +0300

    f

---
+++ lesson02/lesson02adv.js
@@ -0,0 +1,7 @@
+let num = "266219";
+console.log(num[0] * num[1] * num[2] * num[3] * num[4] * num[5]);
+let result = num[0] * num[1] * num[2] * num[3] * num[4] * num[5];
+let cube = result ** 3;
+cube = cube.toString();
+console.log( cube[0]+cube[1]);
+console.log("Hello, world!");
