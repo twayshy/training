1c7ea1a1d4e2b29ace60d908abc02497b3f6d638
Евгения Чедка <twaylik@mail.ru>
2019-07-12 13:08:33 +0300

    lesson06

--- lesson06/script.js
+++ lesson06/script.js
@@ -1,73 +1,86 @@
 'use strict'
 
-let question = prompt ("Какие обязательные ежемесячные расходы у вас есть?");
-let questionAnsw = +prompt ("Во сколько это обойдётся?");
-let questionOne = prompt ("Какие обязательные ежемесячные расходы у вас есть?");
-let questionOneAnsw = +prompt ("Во сколько это обойдётся?");
+let question,
+ questionAnsw,
+ questionOne,
+ questionOneAnsw;
 
+ let money;
+start();
 
-let getExpensesMonth =function(){
-
-    if (isNaN(questionAnsw) || questionAnsw == '' || questionAnsw == null) {
-        return "wrong value 1";
-    }
-    else if (isNaN(questionOneAnsw) || questionOneAnsw == '' || questionOneAnsw == null) {
-       return "wrong value 2";
-    }
-    else {
-        return (questionAnsw + questionOneAnsw);
-    }
-};
-console.log('getExpensesMonth: ', getExpensesMonth());
-
-let money = Number(prompt("Ваш месячный доход???"));
-
-function getAccumulatedMonth() {
-    let result = money- getExpensesMonth();
-    return result;
-};
-let accumulatedMonth = getAccumulatedMonth();
-console.log('accumulatedMonth: ', accumulatedMonth);
-
-
-let mission = 4;
-
-function getTargetMonth() {
-     let result = Math.floor(mission / getAccumulatedMonth());
-    if (result < 0) {
-        return ("Цель не будет достигнута");
-    };
-    return ("Цель будет достигнута через "+result);
-};
-console.log(getTargetMonth());
-
-let budgetDay = Math.floor(accumulatedMonth / 30); 
-
-function getStatusIncome(){
-    if (budgetDay > 800) {
+ let appData = {
+    budget: money,
+    budgetDay: 0,
+    expenses:{},
+    budgetMonth: 0,
+    expensesMonth: 0,
+    asking: function() {
+        question = prompt ("Какие обязательные ежемесячные расходы у вас есть?", "Счёт телефона");
+        while (isNaN(questionAnsw)==true||questionAnsw==null||questionAnsw==""){
+            questionAnsw= +prompt ("Во сколько это обойдётся?", 400);
+        };
+        questionOne = prompt ("Какие обязательные ежемесячные расходы у вас есть?", "Интернет");
+        while(isNaN(questionOneAnsw) ==true||questionOneAnsw===null|| questionOneAnsw===""){
+            questionOneAnsw = +prompt ("Во сколько это обойдётся?", 700);
+        };
+        this.expenses[question]=questionAnsw;
+        this.expenses[questionOne]=questionOneAnsw;
+    },
+    getExpensesMonth: function() {
+        for (let key in appData.expenses){
+            appData.expensesMonth+=appData.expenses[key];
+        }
+    },
+    getBudget: function() {
+       this.budgetMonth=this.budget-this.expensesMonth;
+       this.budgetDay=Math.floor(this.budgetMonth/30);
+    },
+    getTargetMonth: function() {
+        let result = Math.floor(mission / appData.budgetMonth);
+       if (result < 0) {
+           return ("Цель не будет достигнута");
+       };
+       return ("Цель будет достигнута через "+result);
+   },
+   getStatusIncome: function(){
+    if (this.budgetDay > 800) {
         return("Высокий уровень дохода");
     }
-    else if (budgetDay >= 300 && budgetDay <= 800 ){
+    else if (this.budgetDay >= 300 && this.budgetDay <= 800 ){
         return("Средний уровень дохода");
     }
-    else if (budgetDay >= 0 && budgetDay < 300 ) {
+    else if (this.budgetDay >= 0 && this.budgetDay < 300 ) {
         return("Низкий уровень дохода");
     }
-    else if (budgetDay < 0) {
+    else if (this.budgetDay < 0) {
         return "Что-то пошло не так!";
+    };
     }
-}
-console.log('getStatusIncome(): ', getStatusIncome());
+};
+ appData.asking();
+appData.getExpensesMonth();
+appData.getBudget();
+console.log("расходы за месяц "+appData.expensesMonth);
+let mission = 4;
+
+
+console.log(appData.getTargetMonth());
 
 
 
-let start = function(){
-    money = prompt('Ваш месячный доход???');
-    console.log(money);
 
+console.log('getStatusIncome(): ', appData.getStatusIncome());
+
+
+
+function start(){
     do {
-        money = prompt('Ваш месячный доход???');
+        money = Number(prompt('Ваш месячный доход???', 10000));
         console.log(money); 
     } while (isNaN(money) || money == '' || money == null);
 };
-
+let values="";
+for(let key in appData){
+    values+="\n"+key+": "+appData[key];
+}
+console.log("Наша программа включает в себя данные: "+values);
